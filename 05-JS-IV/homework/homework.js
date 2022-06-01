6fae8a6e6938b468bb976a032aef887ae9fa835f
tamaraantonella <tamarafrazzetta@gmail.com>
2022-06-01 11:22:40 -0300

    solving all exercises- JS IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -121,7 +121,10 @@ function pasarUsuarioAPremium (usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
-  
+  for(var i = 0; i < usuarios.length; i++) {
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario (usuario) {
@@ -131,6 +134,11 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  let suma = 0;
+  for (let i = 0; i < usuario.posts.length; i++) {
+    suma += usuario.posts[i].likes;
+  }
+  return suma
 }
 
 function agregarMetodoCalculoDescuento (producto) {
@@ -143,7 +151,12 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function () {
+    let descuento = this.precio * this.porcentajeDeDescuento;
+    let precioReal = this.precio - descuento;
+    return precioReal
+  }
+  return producto
 }
 
 // No modificar nada debajo de esta línea
